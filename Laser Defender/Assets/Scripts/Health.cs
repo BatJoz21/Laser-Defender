88c0af3fe6c09affdfe8c754cf607f1dbf0e20bc
Jozka Siregar <jozkasiregar@gmail.com>
2024-04-22 11:14:35 +0700

    Taking Damage

---
+++ Laser Defender/Assets/Scripts/Health.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Health : MonoBehaviour
+{
+    [SerializeField] private int health = 50;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        DamageDealer damageDealer = collision.GetComponent<DamageDealer>();
+        if (damageDealer != null)
+        {
+            TakeDamage(damageDealer.Damage);
+            damageDealer.Hit();
+        }
+    }
+
+    private void TakeDamage(int damage)
+    {
+        health -= damage;
+        //Debug.Log(health);
+        if (health <= 0)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
